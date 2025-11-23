6da79abd045631ab911635233fb735880994a0ca
fanjatiana-dataS <fanjatianaanita@gmail.com>
2025-11-23 20:50:25 +0300

    sprint6

--- Test/src/main/java/annotations/Server.java
+++ Test/src/main/java/annotations/Server.java
@@ -6,28 +6,16 @@ import method_annotations.Route;
 @Controller
 public class Server {
 
-<<<<<<< HEAD
-    private int server_id;
-    private String server_ip;
-
-    public Server(int id,String server_ip) {
-=======
     private String server_id;
     private String server_ip;
 
     public Server(String id,String server_ip) {
->>>>>>> test_sprint3_bis
         this.server_id=id;
         this.server_ip=server_ip;
     }
 
-<<<<<<< HEAD
-    public int getIdMap() { return this.server_id; }
-    public void setIdMap(int id) { this.server_id = id;}
-=======
     public String getIdMap() { return this.server_id; }
     public void setIdMap(String id) { this.server_id = id;}
->>>>>>> test_sprint3_bis
     public String getMap() {return this.server_ip;}
     public void setMap() {this.server_ip = server_ip;}
 }
--- Test/src/main/java/annotations/ServerController.java
+++ Test/src/main/java/annotations/ServerController.java
@@ -3,23 +3,21 @@ package main.java.annotations;
 import class_annotations.Controller;
 import method_annotations.Route; 
 import view.ModelView;
-import method_annotations.RequestParam;
 
 @Controller
 public class ServerController {
 
     // public ViewController(){}
 
-
-    @Route(value = "/add") 
-    public String addServer(String name, @RequestParam("number") double isa) {
-        return "server: "+name+" successfully added whith "+ isa + "numbers";
-    }
-
     @Route(value = "/servers/{id}") 
     public String get(String id) {
         Server a = new Server(id, "127.0.0.1");
         return a.getMap();
+    }
+
+    @Route(value = "/add") 
+    public String addServer(String name, double number) {
+        return "server: "+name+" successfully added whith "+(number +10) + "numbers";
     }
 
 }
